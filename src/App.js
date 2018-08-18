235391e6ddf9c3fb27747b64adf642fcefdc0964
Alyson Takai <shtakai@gmail.com>
2018-08-18 11:23:39 +0700

    Remove business/businesses

--- src/App.js
+++ src/App.js
@@ -5,27 +5,6 @@ import './App.css';
 import BusinessList from './componensts/BusinessList/BusinessList';
 import SearchBar from './componensts/SearchBar/SearchBar';
 
-const business = {
-  imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90,
-};
-
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business,
-];
-
 class App extends Component {
   render() {
     return (
